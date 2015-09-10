14a835e02bae96c84a42dc5e089db7e932da1b08
Esteban Gerpe Viéitez <stbn694@gmail.com>
2015-09-10 20:10:20 +0200

    Option to tweet the quote

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 		$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(data) {
 			$(".text").text(data.quoteText);
 			$(".author span").text(data.quoteAuthor);
+			var url = "https://twitter.com/intent/tweet?text=\"" + data.quoteText + "\" - " + data.quoteAuthor;
+			$(".quote a").attr("href", url);
 		});
 	}
 
